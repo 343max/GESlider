56e5e2ed6f8e0d6298c05b4dd062f9c9fff3b73f
Max von Webel <max@343max.de>
2014-07-03 15:29:04 +0200

    Initial Commit

---
+++ Slider/GEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GEAppDelegate.h
+//  Slider
+//
+//  Created by Max von Webel on 03/07/14.
+//  Copyright (c) 2014 Max von Webel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Slider/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Slider
+//
+//  Created by Max von Webel on 03/07/14.
+//  Copyright (c) 2014 Max von Webel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GEAppDelegate class]));
+    }
+}
